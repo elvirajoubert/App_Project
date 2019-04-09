12d4c2be0a41b6c25daa955f6fe3c51cf41704ca
Elvira Joubert <reagent01@gmail.com>
2019-04-09 11:21:52 -0500

    initial commit

---
+++ AppPrawject/AppPrawject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppPrawject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
