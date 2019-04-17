ded2959fd2808df4a322344a89cf8bebdfaf1de2
Elvira Joubert <reagent01@gmail.com>
2019-04-17 10:21:08 -0500

    added properties to Pets list

---
+++ AppPrawject/AppPrawject.Model/Model/Pet.cs
@@ -0,0 +1,13 @@
+﻿namespace AppPrawject.Models.Model
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+
+        public string Breed { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ AppPrawject/AppPrawject/Controllers/PetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppPrawject.WebUI.Controllers
+{
+    public class PetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
