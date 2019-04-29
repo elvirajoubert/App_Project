8526d4fefd559880122a9d44a91d37a7c05da74e
Elvira Joubert <reagent01@gmail.com>
2019-04-29 14:57:43 -0500

    added Edit function within the Controller

--- AppPrawject/AppPrawject/Controllers/PetController.cs
+++ AppPrawject/AppPrawject/Controllers/PetController.cs
@@ -58,6 +58,18 @@ namespace AppPrawject.WebUI.Controllers
             return View("Form", pet);// Edit.cshtml, renamed to Form.cshtml
         }
 
+        [HttpPost]
+        public IActionResult Edit(Pet updatedPet)
+        {
+            var oldPet = Pets.Single(p => p.Id == updatedPet.Id);
+            oldPet.Name = updatedPet.Name;
+            oldPet.Breed = updatedPet.Breed;
+            oldPet.Weight = updatedPet.Weight;
+            oldPet.Image = updatedPet.Image;
+
+            return View(nameof(Index), Pets);
+        }
+
 
     }
 }
